9e8d552fdc47fbe59714c4e1ea72b8d9c520df99
Aref Mirhosseini <code@arefmirhosseini.com>
2024-08-19 12:57:50 +0100

    update css

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,11 @@
 <template><RouterView /></template>
+
+<style>
+html,
+body {
+  margin: 2%;
+  padding: 0;
+  font-family: Arial, sans-serif;
+  background-color: #f0f0f0;
+}
+</style>
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <h1>Profiles:</h1>
-    <ul v-if="data">
+    <h1>
+      Profiles <span v-if="data">({{ total }})</span>
+    </h1>
+    <ul class="list" v-if="data">
       <li v-for="(item, index) in data" :key="index">
         <router-link :to="{ name: 'profile', params: { id: item, page: 1 } }">{{
           item
@@ -18,6 +20,7 @@ import api from '../api' // Import your API service
 export default {
   data() {
     return {
+      total: 0,
       data: null
     }
   },
@@ -28,6 +31,7 @@ export default {
     async fetchData() {
       try {
         const response = await api.get('/content/list') // Replace with your actual endpoint
+        this.total = response.data.content.total
         this.data = response.data.content.list
       } catch (error) {
         console.error('Error fetching data:', error)
@@ -37,3 +41,29 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+ul.list {
+  list-style-type: none;
+  padding: 0;
+}
+ul.list li {
+  border: 1px solid #999;
+  border-radius: 10px;
+  padding: 20px;
+  display: inline-block;
+  margin: 5px;
+  background-color: #eee;
+}
+ul.list li:hover {
+  background-color: #ddd;
+}
+ul.list li a {
+  text-decoration: none;
+  color: #333;
+}
+ul.list li a:hover {
+  color: #000;
+  text-decoration: underline;
+}
+</style>
